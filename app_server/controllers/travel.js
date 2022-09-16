e4b2d183bdeb2134f3fc4450bfcf6d9069dda597
blazehalderman <blazehalderman@gmail.com>
2022-09-15 21:09:30 -0700

    adding module 3 json exercise contents

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+
 /* Get travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    pageTitle = process.env.npm_package_description + ' - Travel';
+    res.render('travel', { title: pageTitle, trips });
 };
 
 module.exports = {
